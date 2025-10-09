8080e49600daef9d2ae957b3ca63c373a5492ccc
Robin Tomšů <robin.tomsu@gmail.com>
2025-10-09 20:42:49 +0200

    feat: add background placeholder

--- src/game/GamePanel.java
+++ src/game/GamePanel.java
@@ -4,19 +4,27 @@ import java.awt.*;
 import javax.swing.*;
 
 public class GamePanel extends JPanel {
-    private static final int WIDTH = 960;
-    private static final int HEIGHT = 540;
+    private static final int WIDTH = 1000;
+    private static final int HEIGHT = 1080;
+    private Image backgroundImage;
 
     public GamePanel() {
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         setFocusable(true);
         requestFocusInWindow();
+
+        // Load background image
+        backgroundImage = new ImageIcon(getClass().getResource("/assets/images/background-test.jpg")).getImage();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setColor(new Color(180, 220, 255)); // light-blue background
-        g.fillRect(0, 0, getWidth(), getHeight());
+
+
+        // Draw the background image
+        if (backgroundImage != null) {
+            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+    }
     }
 }
